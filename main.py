50dbed61a31ed6c790fe64b21f8a6c840a6a58ac
cyacedev <github@cyace.dev>
2021-01-06 16:07:50 +0100

    🎨 make graph actually be cubic

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
-from scipy.interpolate import make_interp_spline, BSpline
+from scipy.interpolate import CubicSpline
 
 x=[]
 y=[]
 
 plt.ioff()
-with open('maxsim.csv') as csvfile:
+with open('randomsim.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         if int(row['Iteration'])!= 0:
@@ -15,11 +15,9 @@ with open('maxsim.csv') as csvfile:
             y.append(int(row['CellCount']))
 
 #define spline
-xnew = np.linspace(1, len(x), int(len(x)/10)) 
-spl = make_interp_spline(x, y)
-ynew = spl(xnew)
-
-plt.plot(xnew,ynew, marker='')
+spl = CubicSpline(x, y)
+arr = np.arange(np.amin(x), np.amax(x), 0.1)
+plt.plot(arr, spl(arr), marker='')
 plt.xlabel("Iteration")
 plt.ylabel("Anzahl Zellen")
 plt.title("Verlauf von einer Conway-Simulation")
